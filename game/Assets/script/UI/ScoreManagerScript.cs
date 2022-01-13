53e67037eb746426b2ea9a96144e1baac21591d0
finchinferno <jackfinch88@aol.co.uk>
2022-01-13 22:20:56 +0000

    Inital Commit

---
+++ game/Assets/script/UI/ScoreManagerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreManagerScript : MonoBehaviour
+{
+
+    public GameObject scoreText;
+
+    public int logs = 0;
+
+    private Animator animator;
+
+    void Awake()
+    {
+
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {  
+
+        scoreText.GetComponent<Text>().text = "0";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //allows animator to read the logs perameter as the correct variable
+        animator.SetInteger("logs", logs);
+
+    }
+
+}
